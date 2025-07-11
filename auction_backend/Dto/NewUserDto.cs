e54f8996499537c7d3d7726e7a9f92a82e16e38a
Kaveesha <kaveeshewaduge111@gmail.com>
2025-07-11 15:40:30 +0530

    Jwt Token Configurations

---
+++ auction_backend/Dto/NewUserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace auction_backend.Dto
+{
+    public class NewUserDto
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Token { get; set; }
+    }
+}
--- auction_backend/Program.cs
+++ auction_backend/Program.cs
@@ -1,6 +1,11 @@
 
 using auction_backend.Data;
+using auction_backend.Interfaces;
+using auction_backend.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 namespace auction_backend
 {
@@ -21,6 +26,42 @@ namespace auction_backend
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            //Repo
+            builder.Services.AddIdentity<User, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = true;
+                options.Password.RequireLowercase = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequiredLength = 12;
+            })
+            .AddEntityFrameworkStores<ApplicationDBContext>();
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme =
+                options.DefaultChallengeScheme =
+                options.DefaultForbidScheme =
+                options.DefaultSignInScheme =
+                options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = builder.Configuration["JWT:Issuer"],
+                    ValidateAudience = true,
+                    ValidAudience = builder.Configuration["JWT:Audience"],
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        System.Text.Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"])
+                    )
+                };
+
+            });
+
+            builder.Services.AddScoped<ITokenService, auction_backend.Service.TokenService>();
 
             var app = builder.Build();
 
