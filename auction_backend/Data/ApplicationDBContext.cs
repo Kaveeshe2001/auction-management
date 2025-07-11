9b463304aa9f98dc7d1593198e0972b4b290d68a
Kaveesha <kaveeshewaduge111@gmail.com>
2025-07-11 13:35:44 +0530

    AddDbconnection

---
+++ auction_backend/Data/ApplicationDBContext.cs
@@ -0,0 +1,11 @@
+﻿using auction_backend.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace auction_backend.Data
+{
+    public class ApplicationDBContext: IdentityDbContext<User>
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+    }
+}
---
+++ auction_backend/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace auction_backend.Models
+{
+    [Table("ApplicationUser")]
+    public class User: IdentityUser
+    {
+    }
+}
