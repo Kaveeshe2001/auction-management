2881a4d48fe93cd5ac189c78b2750495d51bae66
Kaveesha <kaveeshewaduge111@gmail.com>
2025-07-15 22:08:17 +0530

    signup

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import Home from "../pages/Home/Home";
 import Categories from "../pages/Categories/Categories";
+import Signup from "../pages/Signup/Signup";
 
 export const router = createBrowserRouter([
     {
@@ -9,6 +10,7 @@ export const router = createBrowserRouter([
         element: <App />,
         children: [
             { index: true, element: <Home /> },
+            { path: 'signup', element: <Signup /> },
             { path: 'categories', element: <Categories /> },
         ]
     }
