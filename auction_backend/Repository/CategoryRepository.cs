55c8dffdd75661aff44bae5abef7ab21b8824fc6
Kaveesha <kaveeshewaduge111@gmail.com>
2025-07-17 22:56:19 +0530

    Category dtos

---
+++ auction_backend/Repository/CategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using auction_backend.Data;
+using auction_backend.Models;
+
+namespace auction_backend.Repository
+{
+    public class CategoryRepository
+    {
+        private readonly ApplicationDBContext _context;
+
+        public CategoryRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Category>> GetAllAsync()
+        {
+            return await _context.Category.ToListAsync();
+        }
+
+    }
+}
