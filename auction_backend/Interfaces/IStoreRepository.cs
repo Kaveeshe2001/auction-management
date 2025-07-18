442f827a84db59f0fedf0bff694ad5413a233fee
Kaveesha <kaveeshewaduge111@gmail.com>
2025-07-18 12:15:52 +0530

    StoreRepository

---
+++ auction_backend/Interfaces/IStoreRepository.cs
@@ -0,0 +1,14 @@
+﻿using auction_backend.Models;
+
+namespace auction_backend.Interfaces
+{
+    public interface IStoreRepository
+    {
+        Task<List<Store>> GetAllAsync();
+        Task<Store?> GetByIdAsync(int id);
+        Task<Store?> GetByUserIdAsync(string userId);
+        Task<Store?> UpdateAsync(int id, Store storeModel);
+        Task<Store> CreateAsync(Store storeModel);
+        Task<Store?> DeleteAsync(int id);
+    }
+}
