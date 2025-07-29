0fe7fdcde2f66998f76a919c6690927568de2648
Kaveesha <kaveeshewaduge111@gmail.com>
2025-07-29 21:07:37 +0530

    feature component

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,11 +1,17 @@
+import { Container } from "react-bootstrap";
 import Banner from "../../components/Home/Banner/Banner";
 import Categories from "../../components/Home/Categories/Categories";
+import Feature from "../../components/Home/Feature/Feature";
 
 function Home() {
     return (
         <>
          <Banner />
          <Categories />
+
+         <Container>
+            <Feature />
+         </Container>
         </>
     );
 };
