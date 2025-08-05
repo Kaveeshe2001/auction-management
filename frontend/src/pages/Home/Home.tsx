a0d194fa09a15098cd944c75e3647d7783db4173
Kaveesha <kaveeshewaduge111@gmail.com>
2025-08-05 19:36:06 +0530

    LatestAuction component

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -2,11 +2,15 @@ import { Container } from "react-bootstrap";
 import Banner from "../../components/Home/Banner/Banner";
 import Categories from "../../components/Home/Categories/Categories";
 import Feature from "../../components/Home/Feature/Feature";
+import LatestAuctions from "../../components/Home/LatestAuctions/LatestAuctions";
 
 function Home() {
     return (
         <>
          <Banner />
+         <Container>
+            <LatestAuctions />
+         </Container>
          <Categories />
 
          <Container>
