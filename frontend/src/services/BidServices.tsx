08bb24095dd36cb0ee1755a0faed782ef927a2f8
Kaveesha <kaveeshewaduge111@gmail.com>
2025-08-05 21:31:34 +0530

    BidServices and Model

---
+++ frontend/src/services/BidServices.tsx
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { BidGet, BidPost } from "../models/Bid";
+import { handleError } from "../handlers/ErrorHandler";
+
+const api = 'http://localhost:5018/auction_backend/bid';
+
+export const placeBidApi = async (auctionId: number, amount: number) => {
+    try {
+        const token = localStorage.getItem('token');
+
+        const data = await axios.post<BidPost>(
+            api,
+            {
+                auctionId: auctionId,
+                amount: amount,
+            },
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+
+        return data;
+    } catch (error) {
+        handleError(error);
+    }
+};
+
+export const getBidsByAuctionApi = async (auctionId: number) => {
+    try {
+        const data = await axios.get<BidGet>(`${api}/auction/${auctionId}`);
+        return data;
+    } catch (error) {
+        handleError(error);
+    }
+};
